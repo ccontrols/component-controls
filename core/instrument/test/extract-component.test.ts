3f4291948bf1dedd0d88558f7f92f69bbb2824c5
atanasster <atanasster@gmail.com>
2020-03-06 11:25:16 -0500

    chore: update tests

--- core/instrument/test/extract-component.test.ts
+++ core/instrument/test/extract-component.test.ts
@@ -5,9 +5,8 @@ import { extractComponent } from '../src/babel/extract-component';
 
 describe('extract-component', () => {
   const folderName = path.join(__dirname, 'examples', 'extract-component');
-  const fileNames = fs
-    .readdirSync(folderName)
-    .filter(fn => fn === 'node-modules.js');
+  const fileNames = fs.readdirSync(folderName);
+  // .filter(fn => fn === 'node-modules.js');
   fileNames.forEach(file => {
     const fileName = path.join(folderName, file);
     it(file, async () => {
