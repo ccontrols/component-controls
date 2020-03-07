5b784e91eb3c689f3febc45c963cf8c731ab115e
atanasster <atanasster@gmail.com>
2020-03-06 19:50:46 -0500

    chore: refactor follow-imports tests

--- core/instrument/test/follow-imports.test.ts
+++ core/instrument/test/follow-imports.test.ts
@@ -1,78 +1,12 @@
+import { loadTestFiles } from './loadTestFiles';
 import { defaultParserOptions, defaultResolveOptions } from '../src/index';
 import { followImports } from '../src/babel/follow-imports';
 
 describe('follow-imports', () => {
-  const extractFollowImportsForFile = (
-    importName: string,
-    fileName: string,
-  ) => {
-    return followImports(importName, require.resolve(fileName), undefined, {
+  loadTestFiles(async (fileName: string) => {
+    return followImports('Button', require.resolve(fileName), undefined, {
       parser: defaultParserOptions,
       resolve: defaultResolveOptions,
     });
-  };
-  it('simple import', () => {
-    expect(
-      extractFollowImportsForFile(
-        'Button',
-        './examples/follow-imports/direct-import.js',
-      ),
-    ).toMatchSnapshot();
-  });
-  it('one level import', () => {
-    expect(
-      extractFollowImportsForFile(
-        'Button',
-        './examples/follow-imports/one-level-import.js',
-      ),
-    ).toMatchSnapshot();
-  });
-  it('one level default import', () => {
-    expect(
-      extractFollowImportsForFile(
-        'Button',
-        './examples/follow-imports/one-level-default-import.js',
-      ),
-    ).toMatchSnapshot();
-  });
-  it('one level namespace import', () => {
-    expect(
-      extractFollowImportsForFile(
-        'Button',
-        './examples/follow-imports/one-level-namespace-import.js',
-      ),
-    ).toMatchSnapshot();
-  });
-  it('one level index import', () => {
-    expect(
-      extractFollowImportsForFile(
-        'Button',
-        './examples/follow-imports/one-level-index-import.js',
-      ),
-    ).toMatchSnapshot();
-  });
-  it('export all', () => {
-    expect(
-      extractFollowImportsForFile(
-        'Button',
-        './examples/follow-imports/export-all.js',
-      ),
-    ).toMatchSnapshot();
-  });
-  it('cjs named export', () => {
-    expect(
-      extractFollowImportsForFile(
-        'Button',
-        './examples/follow-imports/cjs-import.js',
-      ),
-    ).toMatchSnapshot();
-  });
-  it('export node modules', () => {
-    expect(
-      extractFollowImportsForFile(
-        'Button',
-        './examples/follow-imports/button-from-node-nodules.js',
-      ),
-    ).toMatchSnapshot();
-  });
+  }, 'follow-imports');
 });
