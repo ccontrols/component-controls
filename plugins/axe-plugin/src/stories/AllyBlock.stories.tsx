81d6ffb625ca618970b8db3f75773bdc7966de21
atanasster <atanasster@gmail.com>
2020-05-21 16:04:58 -0400

    fix: ally story context

--- plugins/axe-plugin/src/stories/AllyBlock.stories.tsx
+++ plugins/axe-plugin/src/stories/AllyBlock.stories.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Store } from '@component-controls/store';
+import { BlockContextProvider, store } from '@component-controls/blocks';
 import { AxeAllyBlock } from '../index';
-import { MockContext } from '@component-controls/blocks';
+
 export default {
   title: 'Plugins/AxeAllyBlock',
   component: AxeAllyBlock,
 };
 
-export const overview = () => (
-  <MockContext storyId="blocks-core-story-plain--controls">
-    <AxeAllyBlock id="." />
-  </MockContext>
-);
+export const overview = () => {
+  const storyStore = React.useMemo(
+    () => new Store({ store, updateLocalStorage: false }),
+    [],
+  );
+  return (
+    <BlockContextProvider
+      storyId="blocks-core-story-plain--controls"
+      store={storyStore}
+    >
+      <AxeAllyBlock id="." />
+    </BlockContextProvider>
+  );
+};
--- ui/blocks/src/test/index.ts
+++ ui/blocks/src/test/index.ts
@@ -1,1 +1,2 @@
 export * from './MockContext';
+export * from './storyStore';
