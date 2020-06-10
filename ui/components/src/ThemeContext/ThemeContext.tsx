91d2206e1052d16bb5ac312b34dbb87a65f300ee
atanasster <atanasster@gmail.com>
2020-06-10 19:02:36 -0400

    feat: cleanup theme

--- ui/components/src/ThemeContext/ThemeContext.tsx
+++ ui/components/src/ThemeContext/ThemeContext.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import polaris from '@theme-ui/preset-polaris';
-import { get } from '@theme-ui/css';
 import { merge } from 'theme-ui';
-import {
-  ThemeProvider as ThemeUIProvider,
-  Theme,
-  IntrinsicSxElements,
-} from 'theme-ui';
-import { transparentize } from 'polished';
+import { ThemeProvider as ThemeUIProvider, Theme } from 'theme-ui';
 
-import { markdownComponents } from '../Markdown';
-
-type ProvidedComponentsKnownKeys = {
-  [key in keyof IntrinsicSxElements]?: React.ComponentType<any> | string;
-};
-export interface ProvidedComponents extends ProvidedComponentsKnownKeys {
-  [key: string]: React.ComponentType<any> | string | undefined;
-}
-
-export interface ThemeUIProviderProps<Theme> {
-  theme: Partial<Theme> | ((outerTheme: Theme) => Theme);
-  children?: React.ReactNode;
-  components?: ProvidedComponents;
-}
-const ThemeUIProviderExt: <Theme>(
-  props: ThemeUIProviderProps<Theme>,
-) => React.ReactElement = ThemeUIProvider;
+import { markdownComponents, MarkdownComponentType } from '../Markdown';
+import { theme as defaultTheme } from './theme';
 
 export interface ThemeContextProps {
   theme?: Theme;
@@ -37,20 +15,11 @@ export interface ThemeProviderProps {
   /**
    * components to customize the markdown display.
    */
-  components?: ProvidedComponents;
+  components?: MarkdownComponentType;
   dark?: boolean;
   theme?: Theme;
 }
 
-const applyColorMode = (theme: Theme, dark?: boolean) => {
-  if (!dark) {
-    return theme;
-  }
-  const modes = get(theme, 'colors.modes', {});
-  return merge(theme, {
-    colors: get(modes, 'dark', {}),
-  });
-};
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   theme: customTheme,
   children,
@@ -58,153 +27,10 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   components = {},
 }) => {
   const theme = React.useMemo(() => {
-    const defTheme = customTheme || polaris;
-    return applyColorMode(
-      {
-        ...defTheme,
-        defaultBreakpoints: [40, 52, 64].map(n => n + 'em'),
-        initialColorModeName: dark ? 'dark' : 'default',
-        forms: {
-          checkbox: {
-            cursor: 'pointer',
-            border: (t: Theme) => `1px solid ${t?.colors?.text}`,
-            '&:focus': {
-              backgroundColor: 'transarent',
-              boxShadow: (t: Theme) => `0 0 0 2px ${t?.colors?.primary}`,
-              outline: 'none',
-            },
-          },
-        },
-        fonts: {
-          ...defTheme.fonts,
-          serif:
-            'medium-content-title-font,Georgia,Cambria,"Times New Roman",Times,serif',
-        },
-        fontSizes: [12, 14, 16, 20, 24, 32, 42, 64, 96],
-        styles: {
-          ...defTheme.styles,
-          a: {
-            ...defTheme.styles.a,
-            transition: `all 0.3s ease-in-out`,
-          },
-          img: {
-            ...defTheme.styles.img,
-            maxWidth: '100%',
-          },
-          h1: {
-            ...defTheme.styles.h1,
-            fontFamily: 'serif',
-            fontSize: 6,
-          },
-          p: {
-            fontSize: 3,
-            py: 2,
-          },
-          ol: {
-            fontSize: 3,
-          },
-          ul: {
-            fontSize: 3,
-            py: 2,
-          },
-          table: {
-            margin: 0,
-            borderCollapse: 'collapse',
-            fontSize: '14px',
-            lineHeight: '20px',
-            textAlign: 'left',
-            width: '100%',
-            borderSpacing: '0px',
-          },
-          th: {
-            border: 'none',
-            padding: '10px 0 10px 20px',
-          },
-          tbody: {
-            'tr:last-of-type': {
-              borderBottom: 0,
-            },
-          },
-          thead: {
-            borderBottom: '1px solid #999',
-            backgroundColor: 'header',
-            color: 'text',
-          },
-          td: {
-            padding: '16px 20px',
-            borderBottom: 0,
-          },
-          tdgroup: {
-            lineHeight: '24px',
-            background: '#fafbfc',
-            whiteSpace: 'nowrap',
-            padding: '16px 20px',
-            fontWeight: 700,
-            fontFamily: 'monospace',
-          },
-          tr: {
-            borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
-          },
-        },
-        buttons: {
-          primary: {
-            color: '#333',
-            bg: '#f3f3f3',
-            borderRadius: 5,
-            boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 0px 1px inset',
-          },
-          secondary: {
-            bg: 'action',
-          },
-        },
-        cards: {
-          primary: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            padding: 2,
-            borderRadius: 4,
-            boxShadow: '0 0 8px rgba(0, 0, 0, 0.125)',
-          },
-        },
-        links: {
-          nav: {
-            fontWeight: 300,
-            fontSize: '14px',
-            lineHeight: '1.6rem',
-            '&.active': {
-              fontWeight: 700,
-              color: 'primary',
-            },
-          },
-        },
-        colors: {
-          ...polaris.colors,
-          gray: '#f6f6f6',
-          sidebar: '#f6f6f6',
-          header: '#edebe8',
-          highlight: '#d9f2f1',
-          action: '#3B817D',
-          selected: '#027AC5',
-          fadedText: '#69768C',
-          shadow: transparentize(0.9, defTheme.colors.text),
-          modes: {
-            dark: {
-              ...(defTheme.colors.modes ? defTheme.colors.modes.dark : {}),
-              background: '#38404a',
-              sidebar: '#000',
-              text: '#d3d4db',
-              header: '#111111',
-              fadedText: '#c9cacf',
-              selected: '#b3d9ff',
-              action: '#d9f2f1',
-              shadow: transparentize(0.9, '#d3d4db'),
-            },
-          },
-        },
-      },
-      dark,
-    );
+    return {
+      ...(customTheme ? merge(defaultTheme, customTheme) : defaultTheme),
+      initialColorModeName: dark ? 'dark' : 'default',
+    };
   }, [dark, customTheme]);
   return (
     <ThemeContext.Provider
@@ -213,12 +39,13 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
         dark,
       }}
     >
-      <ThemeUIProviderExt
+      <ThemeUIProvider
         theme={theme}
+        //@ts-ignore
         components={{ ...markdownComponents, ...components }}
       >
         {children}
-      </ThemeUIProviderExt>
+      </ThemeUIProvider>
     </ThemeContext.Provider>
   );
 };
---
+++ ui/components/src/ThemeContext/theme.ts
@@ -0,0 +1,213 @@
+import { Theme, useThemeUI } from 'theme-ui';
+const text =
+  'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif';
+
+const heading = {
+  fontFamily: 'heading',
+  lineHeight: 'heading',
+  fontWeight: 'heading',
+};
+
+export const theme: Theme = {
+  colors: {
+    text: '#454f5b',
+    background: '#fff',
+    primary: '#5c6ac4',
+    secondary: '#006fbb',
+    muted: '#e6e6e6',
+    accent: '#f49342',
+    darken: '#00044c',
+    gray: '#f6f6f6',
+    sidebar: '#f6f6f6',
+    header: '#edebe8',
+    highlight: '#d9f2f1',
+    action: '#3B817D',
+    selected: '#027AC5',
+    fadedText: '#69768C',
+    shadow: 'rgba(0, 0, 0, 0.1)',
+    modes: {
+      dark: {
+        primary: '#9c6ade',
+        secondary: '#b4e1fa',
+        highlight: '#b7ecec',
+        muted: '#e6e6e6',
+        background: '#38404a',
+        sidebar: '#000',
+        text: '#d3d4db',
+        header: '#111111',
+        fadedText: '#c9cacf',
+        selected: '#b3d9ff',
+        action: '#d9f2f1',
+        shadow: 'rgba(211, 212, 219, 0.1)',
+      },
+    },
+  },
+  fonts: {
+    body: text,
+    heading:
+      'medium-content-title-font,Georgia,Cambria,"Times New Roman",Times,serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 42, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 600,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.75,
+    heading: 1.25,
+  },
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  buttons: {
+    primary: {
+      color: '#333',
+      backgroundColor: '#f3f3f3',
+      borderRadius: '5px',
+      boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 0px 1px inset',
+    },
+    secondary: {
+      backgroundColor: 'action',
+    },
+  },
+  cards: {
+    primary: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: 2,
+      borderRadius: 4,
+      boxShadow: '0 0 8px rgba(0, 0, 0, 0.125)',
+    },
+  },
+  links: {
+    nav: {
+      fontWeight: 300,
+      fontSize: '14px',
+      lineHeight: '1.6rem',
+      '&.active': {
+        fontWeight: 700,
+        color: 'primary',
+      },
+    },
+  },
+  forms: {
+    checkbox: {
+      cursor: 'pointer',
+      border: (t: Theme) => `1px solid ${t?.colors?.text}`,
+      '&:focus': {
+        backgroundColor: 'transarent',
+        boxShadow: (t: Theme) => `0 0 0 2px ${t?.colors?.primary}`,
+        outline: 'none',
+      },
+    },
+  },
+  styles: {
+    root: {
+      fontFamily: 'body',
+      lineHeight: 'body',
+      fontWeight: 'body',
+    },
+    h1: {
+      ...heading,
+      fontSize: 6,
+    },
+    h2: {
+      ...heading,
+      fontSize: 4,
+    },
+    h3: {
+      ...heading,
+      fontSize: 3,
+    },
+    h4: {
+      ...heading,
+      fontSize: 2,
+    },
+    h5: {
+      ...heading,
+      fontSize: 1,
+    },
+    h6: {
+      ...heading,
+      fontSize: 0,
+    },
+    a: {
+      color: 'primary',
+      transition: `all 0.3s ease-in-out`,
+    },
+    pre: {
+      fontFamily: 'monospace',
+      overflowX: 'auto',
+      code: {
+        color: 'inherit',
+      },
+    },
+    code: {
+      fontFamily: 'monospace',
+      fontSize: 'inherit',
+    },
+    img: {
+      maxWidth: '100%',
+    },
+    p: {
+      fontSize: 3,
+      py: 2,
+      color: 'text',
+      fontFamily: 'body',
+      fontWeight: 'body',
+      lineHeight: 'body',
+    },
+    ol: {
+      fontSize: 3,
+    },
+    ul: {
+      fontSize: 3,
+      py: 2,
+    },
+    table: {
+      margin: 0,
+      borderCollapse: 'collapse',
+      fontSize: '14px',
+      lineHeight: '20px',
+      textAlign: 'left',
+      width: '100%',
+      borderSpacing: 0,
+    },
+    th: {
+      border: 'none',
+      padding: '10px 0 10px 20px',
+    },
+    //@ts-ignore
+    tbody: {
+      'tr:last-of-type': {
+        borderBottom: 0,
+      },
+    },
+    thead: {
+      borderBottom: '1px solid #999',
+      backgroundColor: 'header',
+      color: 'text',
+    },
+    td: {
+      padding: '16px 20px',
+      borderBottom: 0,
+    },
+    tdgroup: {
+      lineHeight: '24px',
+      background: '#fafbfc',
+      whiteSpace: 'nowrap',
+      padding: '16px 20px',
+      fontWeight: 700,
+      fontFamily: 'monospace',
+    },
+    tr: {
+      borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+    },
+  },
+};
+
+export const useTheme = (): Theme => {
+  const { theme: currentTheme } = useThemeUI();
+  return currentTheme || theme;
+};
