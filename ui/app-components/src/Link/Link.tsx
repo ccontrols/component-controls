a78ef698731e2d8623ac8b2e30424659955939de
atanasster <atanasster@gmail.com>
2020-06-15 17:59:31 -0400

    fix: local path regex

--- ui/app-components/src/Link/Link.tsx
+++ ui/app-components/src/Link/Link.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { LinkProps } from 'theme-ui';
 import { ExternalLink } from '@component-controls/components';
 import { useGetLinkClass } from './LinkContext';
 export const Link: FC<LinkProps> = props => {
   const { href } = props;
-  if (typeof href === 'string' && !/\/$/.test(href)) {
+  //https://stackoverflow.com/questions/10687099/how-to-test-if-a-url-string-is-absolute-or-relative/10687158
+  const r = useMemo(() => new RegExp('^(?:[a-z]+:)?//', 'i'), []);
+  if (typeof href === 'string' && r.test(href)) {
     //@ts-ignore
     return <ExternalLink {...props} />;
   }
