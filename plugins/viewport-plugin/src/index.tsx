fa633a647c37ebc4ed81a6ba4e80a37499df1d6a
@mas764 <mas764@cornell.edu>
2020-06-21 17:15:55 -0400

    chore: more theming

--- plugins/viewport-plugin/src/index.tsx
+++ plugins/viewport-plugin/src/index.tsx
@@ -1,45 +1,60 @@
 /** @jsx jsx */
 import { FC } from 'react';
-import { Box, jsx } from 'theme-ui';
+import { Box, Theme, jsx } from 'theme-ui';
+import {
+  StoryBlockContainer,
+  Story,
+  StoryBlockContainerProps,
+} from '@component-controls/blocks';
 
-export const ViewportBlock: FC = () => {
+export const ViewportBlock: FC<StoryBlockContainerProps> = props => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        minHeight: '250px',
-        height: '400px',
-        width: '100%',
-        overflow: 'auto',
-        p: 1,
-        bg: 'gray',
-      }}
-    >
-      <Box
-        sx={{
-          minWidth: '250px',
-          height: '500px',
-          margin: '5px',
-          background: 'green',
-        }}
-      ></Box>
-      <Box
-        sx={{
-          minWidth: '500px',
-          height: '500px',
-          margin: '5px',
-          background: 'black',
-        }}
-      ></Box>
-      <Box
-        sx={{
-          minWidth: '900px',
-          height: '500px',
-          margin: '5px',
-          background: 'gray',
-        }}
-      ></Box>
-    </Box>
+    <StoryBlockContainer {...props}>
+      {({ story: { id: storyId } = {} }) => (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            minHeight: '250px',
+            height: '400px',
+            width: '100%',
+            overflow: 'auto',
+            p: 1,
+            boxShadow: (t: Theme) => `0px 2px 6px 0px ${t.colors?.shadow}`,
+          }}
+        >
+          <Box
+            sx={{
+              minWidth: '250px',
+              height: '500px',
+              m: 1,
+              boxShadow: (t: Theme) => `0px 2px 6px 0px ${t.colors?.shadow}`,
+            }}
+          >
+            <Story id={storyId} />
+          </Box>
+          <Box
+            sx={{
+              minWidth: '500px',
+              height: '500px',
+              m: 1,
+              boxShadow: (t: Theme) => `0px 2px 6px 0px ${t.colors?.shadow}`,
+            }}
+          >
+            <Story id={storyId} />
+          </Box>
+          <Box
+            sx={{
+              minWidth: '900px',
+              height: '500px',
+              m: 1,
+              boxShadow: (t: Theme) => `0px 2px 6px 0px ${t.colors?.shadow}`,
+            }}
+          >
+            <Story id={storyId} />
+          </Box>
+        </Box>
+      )}
+    </StoryBlockContainer>
   );
 };
