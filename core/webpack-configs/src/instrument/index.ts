826c1b8bf03c5ef67eeade769ea13ab9c62dedf1
atanasster <atanasster@gmail.com>
2020-06-26 13:04:11 -0400

    fix: webpack configs inherit mdx

--- core/webpack-configs/src/instrument/index.ts
+++ core/webpack-configs/src/instrument/index.ts
@@ -17,25 +17,23 @@ export const instrument: Configuration = {
       {
         test: /\.(md|mdx)$/,
         exclude: [/node_modules/],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [require.resolve('@babel/preset-env'), { modules: 'commonjs' }],
-                require.resolve('@babel/preset-react'),
-              ],
-            },
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [require.resolve('@babel/preset-env'), { modules: 'commonjs' }],
+            require.resolve('@babel/preset-react'),
+          ],
+        },
+      },
+      {
+        test: /\.(md|mdx)$/,
+        exclude: [/node_modules/],
+        loader: '@component-controls/loader/loader',
+        options: {
+          mdx: {
+            transformMDX: true,
           },
-          {
-            loader: '@component-controls/loader/loader',
-            options: {
-              mdx: {
-                transformMDX: true,
-              },
-            },
-          },
-        ],
+        },
       },
       {
         test: /\.(story|stories).(js|jsx|ts|tsx)$/,
