0d8e43ae8ff6f8101cb6787182a4c570e5ab0644
atanasster <atanasster@gmail.com>
2020-08-09 13:29:16 +0300

    test: update snapshots

--- core/instrument/test/package-info.test.ts
+++ core/instrument/test/package-info.test.ts
@@ -5,13 +5,14 @@ import { packageInfo } from '../src/misc/package-info';
 describe('package-info', () => {
   it('current folder', async () => {
     expect(
-      await packageInfo(__dirname, defaultPackageOptions),
+      await packageInfo('name', __dirname, defaultPackageOptions),
     ).toMatchSnapshot();
   });
 
   it('no package', async () => {
     expect(
       await packageInfo(
+        'name',
         path.resolve(__dirname, '..', '..', '..'),
         defaultPackageOptions,
       ),
@@ -21,6 +22,7 @@ describe('package-info', () => {
   it('error fields', async () => {
     expect(
       await packageInfo(
+        'name',
         path.resolve(__dirname, 'fixtures', 'package-info'),
         defaultPackageOptions,
       ),
