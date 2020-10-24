6a96e6fbf3071895a49f4b906e9852c6ca22d13c
atanasster <atanasster@gmail.com>
2020-10-24 01:26:48 -0400

    test: update tests

--- core/webpack-configs/tests/instrument.test.ts
+++ core/webpack-configs/tests/instrument.test.ts
@@ -3,32 +3,35 @@ import { getWebpackConfig } from '../src/index';
 describe('merge instrument and react-docgen-typescript', () => {
   it('get config', () => {
     expect(
-      getWebpackConfig(['instrument', 'react-docgen-typescript']),
+      getWebpackConfig(['instrument', 'react-docgen-typescript'], {}),
     ).toMatchSnapshot();
   });
   it('merge options', () => {
     expect(
-      getWebpackConfig([
-        'instrument',
-        {
-          name: 'react-docgen-typescript',
-          config: {
-            module: {
-              rules: [
-                {
-                  loader: '@component-controls/loader/loader',
-                  options: {
-                    //instrumentation options
-                    prettier: {
-                      tabWidth: 4,
+      getWebpackConfig(
+        [
+          'instrument',
+          {
+            name: 'react-docgen-typescript',
+            config: {
+              module: {
+                rules: [
+                  {
+                    loader: '@component-controls/loader/loader',
+                    options: {
+                      //instrumentation options
+                      prettier: {
+                        tabWidth: 4,
+                      },
                     },
                   },
-                },
-              ],
+                ],
+              },
             },
           },
-        },
-      ]),
+        ],
+        {},
+      ),
     ).toMatchSnapshot();
   });
 });
--- core/webpack-configs/tests/react-typescript.test.ts
+++ core/webpack-configs/tests/react-typescript.test.ts
@@ -2,30 +2,33 @@ import { getWebpackConfig } from '../src/index';
 
 describe('react-docgen-typescript', () => {
   it('get config', () => {
-    expect(getWebpackConfig(['react-docgen-typescript'])).toMatchSnapshot();
+    expect(getWebpackConfig(['react-docgen-typescript'], {})).toMatchSnapshot();
   });
   it('merge options', () => {
     expect(
-      getWebpackConfig([
-        {
-          name: 'react-docgen-typescript',
-          config: {
-            module: {
-              rules: [
-                {
-                  loader: '@component-controls/loader/loader',
-                  options: {
-                    //instrumentation options
-                    prettier: {
-                      tabWidth: 4,
+      getWebpackConfig(
+        [
+          {
+            name: 'react-docgen-typescript',
+            config: {
+              module: {
+                rules: [
+                  {
+                    loader: '@component-controls/loader/loader',
+                    options: {
+                      //instrumentation options
+                      prettier: {
+                        tabWidth: 4,
+                      },
                     },
                   },
-                },
-              ],
+                ],
+              },
             },
           },
-        },
-      ]),
+        ],
+        {},
+      ),
     ).toMatchSnapshot();
   });
 });
