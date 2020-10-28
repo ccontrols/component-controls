0427ee010c44f67f4b1f809078dcc62298fecc84
atanasster <atanasster@gmail.com>
2020-10-28 14:27:38 -0400

    chore: core/logger

--- core/logger/src/index.ts
+++ core/logger/src/index.ts
@@ -18,17 +18,17 @@ const output = (heading: string, text: string) => {
   console.log(chalk.bgRgb(...logOptions.colors)(`@${heading}`), text);
 };
 
-export const log = (heading: string, text: string) => {
+export const log = (heading: string, text: string): void => {
   if (logOptions.logLevel === 'all') {
     output(heading, text);
   }
 };
-export const error = (heading: string, text: string) => {
+export const error = (heading: string, text: string): void => {
   if (logOptions.logLevel !== 'none') {
     output(heading, text);
   }
 };
 
-export const setLogOptions = (options: Partial<LogOptions> = {}) => {
+export const setLogOptions = (options: Partial<LogOptions> = {}): void => {
   logOptions = { ...logOptions, ...options };
 };
