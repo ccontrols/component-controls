26780b401e7dcfa80108c4da53c4a8fbb8cbf7cf
atanasster <atanasster@gmail.com>
2020-10-28 15:17:52 -0400

    test: update tests

---
+++ core/config/test/fixtures/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  addons: [
+    {
+      name: '@component-controls/storybook',
+      options: {
+        webpack: ['instrument', 'react-docgen-typescript'],
+      },
+    },
+  ],
+  stories: [
+    '../../../../ui/editors/src/**/*.stories.(js|jsx|tsx|mdx)',
+    '../../../../ui/components/src/**/*.stories.(js|jsx|tsx|mdx)',
+    '../../../../ui/blocks/src/**/*.stories.(js|jsx|tsx|mdx)',
+    '../../../../examples/stories/src/**/*.stories.(js|jsx|tsx|mdx)',
+  ],
+};
