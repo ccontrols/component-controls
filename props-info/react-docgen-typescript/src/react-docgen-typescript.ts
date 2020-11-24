6ce277adcb7e578f88c60f40b9379d4816b72137
atanasster <atanasster@gmail.com>
2020-11-24 01:51:14 -0500

    feat: typescript props read compiler options

--- props-info/react-docgen-typescript/src/react-docgen-typescript.ts
+++ props-info/react-docgen-typescript/src/react-docgen-typescript.ts
@@ -1,4 +1,14 @@
-import { ParserOptions, withDefaultConfig } from 'react-docgen-typescript';
+import { ParserOptions, withCompilerOptions } from 'react-docgen-typescript';
+import path from 'path';
+import { findUpFile } from '@component-controls/core/node-utils';
+import {
+  ScriptTarget,
+  ModuleKind,
+  JsxEmit,
+  readConfigFile,
+  sys,
+  parseJsonConfigFileContent,
+} from 'typescript';
 import { computeComponentName } from './defaults';
 import { RectDocgenTypescriptOptions } from './types';
 
@@ -19,16 +29,40 @@ export const extractDocgenTypescriptInfo = (
     },
     componentNameResolver = computeComponentName,
     shouldExtractLiteralValuesFromEnum = true,
+    shouldRemoveUndefinedFromOptional = true,
     savePropValueAsString,
   } = reactDocGenTypescriptOptions || {};
-
   const parserOptions: ParserOptions = {
     propFilter,
     componentNameResolver,
     shouldExtractLiteralValuesFromEnum,
+    shouldRemoveUndefinedFromOptional,
     savePropValueAsString,
   };
-  const parser = withDefaultConfig(parserOptions);
+
+  let compilerConfig = {
+    jsx: JsxEmit.React,
+    module: ModuleKind.CommonJS,
+    target: ScriptTarget.Latest,
+  };
+  const tsConfigFile = findUpFile(path.dirname(fileName), 'tsconfig.json');
+  if (tsConfigFile) {
+    try {
+      const configContent = readConfigFile(tsConfigFile, sys.readFile);
+      const config = parseJsonConfigFileContent(
+        configContent.config,
+        sys,
+        path.dirname(tsConfigFile),
+        {},
+        tsConfigFile,
+      );
+      compilerConfig = {
+        ...compilerConfig,
+        ...config.options,
+      };
+    } catch (e) {}
+  }
+  const parser = withCompilerOptions(compilerConfig, parserOptions);
   try {
     const docgenInfo = parser.parse(fileName);
     if (Array.isArray(docgenInfo) && docgenInfo.length > 0) {
