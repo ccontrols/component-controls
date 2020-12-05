72ab2986aaff728ffe8d9085c0504897defc1d1e
atanasster <atanasster@gmail.com>
2020-12-05 11:29:23 -0500

    fix: emotion alias for sb manager

--- integrations/storybook/src/preset.ts
+++ integrations/storybook/src/preset.ts
@@ -1,15 +1,23 @@
-import path from 'path';
+import { RuleSetRule } from 'webpack';
 import { mergeWebpackConfig } from '@component-controls/webpack-configs';
 const LoaderPlugin = require('@component-controls/loader/plugin');
 import { PresetOptions, defaultRules } from './types';
-
-const toPath = (p: string) => path.join(process.cwd(), p);
 
 module.exports = {
   config: (entry: any[] = []) => {
     const result = [...entry];
     result.push(require.resolve('./config'));
     return result;
+  },
+  babel: async (entry: any, options: any) => {
+    if (options._version >= '6.0.0') {
+      return {
+        ...entry,
+        plugins: [...entry.plugins, '@emotion'],
+      };
+    }
+    return entry;
+    // any extra options you want to set
   },
   addons: (entry: any = {}) => {
     const { pages: customPages } = entry;
@@ -45,7 +53,19 @@ module.exports = {
 
     return result;
   },
-
+  managerWebpack: (config: any) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': require.resolve('@emotion/react'),
+          'emotion-theming': require.resolve('@emotion/react'),
+        },
+      },
+    };
+  },
   webpackFinal: (config: any = {}, options: PresetOptions = {}) => {
     const mergedConfig = mergeWebpackConfig(
       config,
@@ -59,13 +79,47 @@ module.exports = {
         new LoaderPlugin({ defaultConfigPath: '.storybook' }),
       ],
     };
-    if (config._version > '6.0.0') {
+    if (newConfig.module) {
+      newConfig.module = {
+        ...newConfig.module,
+        rules: newConfig.module.rules.map(r => {
+          return Array.isArray(r.use)
+            ? {
+                ...r,
+                use: r.use.map(use =>
+                  (use as RuleSetRule).options && (use as RuleSetRule).options
+                    ? {
+                        ...(use as RuleSetRule),
+                        options: {
+                          ...(use as any).options,
+                          presets: Array.isArray((use as any).options?.presets)
+                            ? (use as any).options.presets.map(
+                                (preset: any) => {
+                                  return Array.isArray(preset)
+                                    ? preset.map(p => {
+                                        return p.runtime
+                                          ? { ...p, runtime: 'classic' }
+                                          : p;
+                                      })
+                                    : preset;
+                                },
+                              )
+                            : undefined,
+                        },
+                      }
+                    : use,
+                ),
+              }
+            : r;
+        }) as RuleSetRule[],
+      };
+
       newConfig.resolve = {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          'emotion-theming': toPath('node_modules/@emotion/react'),
+          '@emotion/core': require.resolve('@emotion/react'),
+          'emotion-theming': require.resolve('@emotion/react'),
         },
       };
     }
