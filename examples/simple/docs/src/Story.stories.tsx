91cb7718b77bb5f6e04108a7f4c4d212b6a0591a
atanasster <atanasster@gmail.com>
2021-01-11 21:02:18 -0800

    docs: update types

--- examples/simple/docs/src/Story.stories.tsx
+++ examples/simple/docs/src/Story.stories.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Document, Example } from '@component-controls/core';
 import { Component } from '../../src/component';
 
 export default {
   title: 'Main story',
   component: Component,
-};
+} as Document;
 
-export const main = () => <Component title="some text" />;
+export const main: Example = () => <Component title="some text" />;
