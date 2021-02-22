50ad9e1ad5ca74ec0844c84674ec3ff64085f071
atanasster <atanasster@gmail.com>
2021-02-22 18:48:05 -0500

    fix: rray colors handling

--- ui/components/src/Tag/Tag.stories.tsx
+++ ui/components/src/Tag/Tag.stories.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Document, Example, ControlTypes } from '@component-controls/core';
+import { tailwind } from '@theme-ui/presets';
+import { ThemeProvider } from '../ThemeContext/ThemeContext';
 import { Tag, TagProps } from './Tag';
 
 export default {
@@ -14,4 +16,12 @@ export const overview: Example<TagProps> = ({ color }) => {
 
 overview.controls = {
   color: { type: ControlTypes.COLOR, value: 'red' },
+};
+
+export const twilwindArrayColors: Example<TagProps> = () => {
+  return (
+    <ThemeProvider theme={tailwind as any}>
+      <Tag color="red">some text</Tag>
+    </ThemeProvider>
+  );
 };
