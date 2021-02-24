966324a2cb271c264c64824446f87397c3d178aa
atanasster <atanasster@gmail.com>
2021-02-23 20:41:33 -0500

    fix: fix confi tabs

--- examples/starter/.config/runtime.tsx
+++ examples/starter/.config/runtime.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { RunOnlyConfiguration, defaultRunConfig } from "@component-controls/core";
-import { TestingPage } from "./TestingPage";
+import {
+  RunOnlyConfiguration,
+  defaultRunConfig,
+} from '@component-controls/core';
+import { TestingPage } from './TestingPage';
 
 const config: RunOnlyConfiguration = {
   title: `awLib`,
   description: `Some description meta.`,
   author: 'my name',
-  pages: {
-    story: {
-      tabs: [
-        ...defaultRunConfig.pages.story.tabs,
-        { title: 'Testing', render: () => <TestingPage /> },
-      ],
-    },
-  },
-
 };
 
 export default config;
