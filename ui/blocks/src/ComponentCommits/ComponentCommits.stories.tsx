d0c68d76138ca9c0cbaef76de8369b416427724e
martin-stoyanov <martin.a.stoyanov@gmail.com>
2021-03-10 16:29:55 -0500

    docs: add martin as author

--- ui/blocks/src/ComponentCommits/ComponentCommits.stories.tsx
+++ ui/blocks/src/ComponentCommits/ComponentCommits.stories.tsx
@@ -7,6 +7,7 @@ export default {
   title: 'Blocks/ComponentCommits',
   component: ComponentCommits,
   category: ' Component',
+  author: 'martin-stoyanov',
   decorators: makeDecorators('blocks-core-story-plain--controls'),
 } as Document;
 
