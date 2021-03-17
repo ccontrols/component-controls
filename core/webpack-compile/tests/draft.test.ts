dd2a1bfd788914e7119769697a7290e6e6a429b3
atanasster <atanasster@gmail.com>
2021-03-17 07:12:26 -0400

    test: convert webpack-compile snapshots to functional

--- core/webpack-compile/tests/draft.test.ts
+++ core/webpack-compile/tests/draft.test.ts
@@ -11,6 +11,9 @@ expect.addSnapshotSerializer({
 describe('test draft', () => {
   it('compile', async () => {
     const { store } = await compile({
+      logOptions: {
+        logLevel: 'none',
+      },
       presets: ['react', 'react-docgen-typescript'],
       //configPath: path.resolve(__dirname, 'fixtures', 'draft'),
       configPath: path.resolve(
@@ -20,6 +23,465 @@ describe('test draft', () => {
         '.config',
       ),
     });
-    expect(store).toMatchSnapshot();
+    const store1 = store.stores.find(
+      store => store.doc.title === 'Library/Components/Button',
+    );
+    expect(store1).toMatchObject({
+      stories: {
+        overview: {
+          name: 'overview',
+          id: 'overview',
+          source: '() => <Button>theme-ui</Button>',
+        },
+      },
+      doc: {
+        title: 'Library/Components/Button',
+        order: 2,
+        component: 'Button',
+      },
+    });
+
+    const store2 = store.stores.find(store => store.doc.title === 'First blog');
+    expect(store2).toMatchObject({
+      stories: {},
+      doc: {
+        type: 'blog',
+        title: 'First blog',
+      },
+    });
+    const store3 = store.stores.find(store => store.doc.title === 'First Page');
+    expect(store3).toMatchObject({
+      stories: {},
+      doc: {
+        title: 'First Page',
+      },
+    });
+
+    const store4 = store.stores.find(
+      store => store.doc.title === 'Library/Components/MDX',
+    );
+    expect(store4).toMatchObject({
+      stories: {
+        overview: {
+          name: 'overview',
+          component: 'Spinner',
+          id: 'overview',
+          loc: {
+            start: {
+              column: 9,
+              line: 13,
+            },
+            end: {
+              column: 65,
+              line: 13,
+            },
+          },
+          arguments: [
+            {
+              value: [
+                {
+                  value: 'color',
+                  name: 'color',
+                  loc: {
+                    start: {
+                      line: 0,
+                      column: 3,
+                    },
+                    end: {
+                      line: 0,
+                      column: 8,
+                    },
+                  },
+                  usage: [
+                    {
+                      loc: {
+                        start: {
+                          line: 0,
+                          column: 31,
+                        },
+                        end: {
+                          line: 0,
+                          column: 36,
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+              name: undefined,
+              loc: {
+                start: {
+                  line: 0,
+                  column: 1,
+                },
+                end: {
+                  line: 0,
+                  column: 10,
+                },
+              },
+            },
+          ],
+          source: '({ color }) => <Spinner color={color}>click me</Spinner>',
+        },
+      },
+      doc: {
+        order: 0,
+        title: 'Library/Components/MDX',
+      },
+    });
+
+    expect(store.components).toMatchObject({
+      ef6966a6426a2258b8bb2a8cd8daf14e: {
+        name: 'Spinner',
+        from: '../components/Spinner',
+        externalDependencies: {
+          react: [
+            {
+              name: 'React',
+              importedName: 'default',
+            },
+          ],
+          polished: [
+            {
+              name: 'lighten',
+              importedName: 'lighten',
+            },
+          ],
+          'prop-types': [
+            {
+              name: 'PropTypes',
+              importedName: 'default',
+            },
+          ],
+        },
+        importedName: 'Spinner',
+        jsx: [
+          {
+            children: [],
+            name: 'button',
+            attributes: ['type', 'disabled', 'onClick', 'style'],
+          },
+        ],
+        fileName: 'Spinner.js',
+        source:
+          "import React from 'react';\nimport { lighten } from 'polished';\nimport PropTypes from 'prop-types';\n\n/**\n * Spinner with react PropTypes\n */\nexport const Spinner = ({\n  disabled,\n  children,\n  onClick,\n  style,\n  backgroundColor,\n  color,\n  type,\n  padding,\n}) => (\n  <button\n    type={type}\n    disabled={disabled}\n    onClick={onClick}\n    style={{\n      ...style,\n      backgroundColor,\n      color: lighten(disabled ? 0.4 : 0, color),\n      padding,\n    }}\n  >\n    {children}\n  </button>\n);\n\nSpinner.defaultProps = {\n  disabled: false,\n  children: 'default',\n  onClick: () => {},\n  style: {},\n  backgroundColor: '#fefefe',\n  color: 'black',\n  type: 'button',\n  padding: 5,\n};\n\nSpinner.propTypes = {\n  /**\n   * Boolean indicating whether the button should render as disabled\n   */\n  disabled: PropTypes.bool,\n\n  /**\n   * button label\n   */\n  children: PropTypes.string,\n\n  /**\n   * onClick handler\n   */\n  onClick: PropTypes.func,\n\n  /**\n   * Custom styles\n   */\n  style: PropTypes.shape({}),\n\n  /**\n   * Background color\n   */\n  backgroundColor: PropTypes.string,\n\n  /**\n   * Text color, default black\n   */\n  color: PropTypes.string,\n\n  /**\n   * Button type\n   */\n  type: PropTypes.oneOf(['button', 'reset', 'submit']),\n\n  /**\n   * Numeric  field type\n   */\n  padding: PropTypes.number,\n};\n",
+        info: {
+          description: 'Spinner with react PropTypes',
+          displayName: 'Spinner',
+          methods: [],
+          props: {
+            disabled: {
+              description:
+                'Boolean indicating whether the button should render as disabled',
+              defaultValue: 'false',
+              type: {
+                name: 'boolean',
+              },
+            },
+            children: {
+              description: 'button label',
+              defaultValue: "'default'",
+              type: {
+                name: 'string',
+                required: false,
+              },
+            },
+            onClick: {
+              description: 'onClick handler',
+              defaultValue: '() => {}',
+              type: {
+                name: 'function',
+                required: false,
+              },
+            },
+            style: {
+              description: 'Custom styles',
+              defaultValue: '{}',
+              type: {
+                name: 'object',
+                value: [],
+              },
+            },
+            backgroundColor: {
+              description: 'Background color',
+              defaultValue: "'#fefefe'",
+              type: {
+                name: 'string',
+                required: false,
+              },
+            },
+            color: {
+              description: 'Text color, default black',
+              defaultValue: "'black'",
+              type: {
+                name: 'string',
+                required: false,
+              },
+            },
+            type: {
+              description: 'Button type',
+              defaultValue: "'button'",
+              type: {
+                name: 'enum',
+                value: [
+                  {
+                    name: 'string',
+                    value: "'button'",
+                  },
+                  {
+                    name: 'string',
+                    value: "'reset'",
+                  },
+                  {
+                    name: 'string',
+                    value: "'submit'",
+                  },
+                ],
+              },
+            },
+            padding: {
+              description: 'Numeric  field type',
+              defaultValue: '5',
+              type: {
+                name: 'number',
+                required: false,
+              },
+            },
+          },
+        },
+        fileInfo: {
+          sloc: {
+            total: 84,
+            source: 47,
+            comment: 27,
+            single: 0,
+            block: 27,
+            mixed: 0,
+            empty: 10,
+            todo: 0,
+            blockEmpty: 0,
+          },
+          commits: [
+            {
+              hash: '3e9724f3d740da33bd277b03f8a83017159be114',
+              subject: 'docs: added starter example',
+              authorName: 'atanasster',
+              authorDate: '2020-07-19T04:05:43.000Z',
+              authorEmail: 'atanasster@gmail.com',
+              committerName: 'atanasster',
+              committerDate: '2020-07-19T04:05:43.000Z',
+              committerEmail: 'atanasster@gmail.com',
+            },
+          ],
+        },
+      },
+      '5a3b7658720a0f4b8c0fd61ba5fb8c34': {
+        name: 'Button',
+        from: '../components/Button',
+        externalDependencies: {
+          react: [
+            {
+              name: 'React',
+              importedName: 'default',
+            },
+            {
+              name: 'FC',
+              importedName: 'FC',
+            },
+          ],
+        },
+        importedName: 'Button',
+        jsx: [
+          {
+            children: [],
+            name: 'button',
+            attributes: ['type', 'disabled', 'onClick', 'style'],
+          },
+        ],
+        fileName: 'Button.tsx',
+        source:
+          "import React, { FC } from 'react';\n\nexport interface ButtonProps {\n  /**\n   *  Boolean indicating whether the button should render as disabled\n   */\n  disabled?: boolean;\n\n  /**\n   * Background color\n   */\n  backgroundColor?: string;\n\n  /**\n   * Text color, default black\n   */\n  color?: string;\n\n  /**\n   * Numeric  field type\n   */\n  padding?: number;\n\n  /**\n   * Button type\n   */\n  type?: 'button' | 'reset' | 'submit';\n\n  /**\n   * Custom styles\n   */\n  style?: object;\n  /**\n   * button label\n   */\n  children?: string;\n\n  /**\n   * onClick handler\n   */\n  onClick?: () => void;\n}\n\n/**\n * Button with react Typescript properties\n */\nexport const Button: FC<ButtonProps> = ({\n  disabled,\n  children,\n  onClick,\n  style,\n  backgroundColor,\n  color,\n  type,\n  padding,\n}) => (\n  <button\n    type={type}\n    disabled={disabled}\n    onClick={onClick}\n    style={{\n      ...style,\n      backgroundColor,\n      color,\n      padding,\n    }}\n  >\n    {children}\n  </button>\n);\n\nButton.defaultProps = {\n  disabled: false,\n  children: 'default',\n  onClick: () => {},\n  style: {},\n  backgroundColor: '#fefefe',\n  color: 'black',\n  type: 'button',\n  padding: 5,\n};\n",
+        info: {
+          tags: {},
+          description: 'Button with react Typescript properties',
+          displayName: 'Button',
+          methods: [],
+          props: {
+            disabled: {
+              description:
+                'Boolean indicating whether the button should render as disabled',
+              parentName: 'ButtonProps',
+              defaultValue: false,
+              type: {
+                name: 'boolean',
+                raw: 'boolean',
+              },
+            },
+            backgroundColor: {
+              description: 'Background color',
+              parentName: 'ButtonProps',
+              defaultValue: '#fefefe',
+              type: {
+                name: 'string',
+                raw: 'string',
+              },
+            },
+            color: {
+              description: 'Text color, default black',
+              parentName: 'ButtonProps',
+              defaultValue: 'black',
+              type: {
+                name: 'string',
+                raw: 'string',
+              },
+            },
+            padding: {
+              description: 'Numeric  field type',
+              parentName: 'ButtonProps',
+              defaultValue: 5,
+              type: {
+                name: 'number',
+                raw: 'number',
+              },
+            },
+            type: {
+              description: 'Button type',
+              parentName: 'ButtonProps',
+              defaultValue: 'button',
+              type: {
+                name: 'union',
+                value: [
+                  {
+                    name: 'button',
+                  },
+                  {
+                    name: 'reset',
+                  },
+                  {
+                    name: 'submit',
+                  },
+                ],
+                raw: '"button" | "reset" | "submit"',
+              },
+            },
+            style: {
+              description: 'Custom styles',
+              parentName: 'ButtonProps',
+              defaultValue: '{}',
+              type: {
+                name: 'object',
+                raw: 'object',
+              },
+            },
+            children: {
+              description: 'button label',
+              parentName: 'ButtonProps',
+              defaultValue: 'default',
+              type: {
+                name: 'function',
+                raw:
+                  'string | (string & {}) | (string & ReactElement<any, string | ((props: any) => ReactElement<any, any> | null) | (new (props: any) => Component<any, any, any>)>) | (string & ReactNodeArray) | (string & ReactPortal)',
+              },
+            },
+            onClick: {
+              description: 'onClick handler',
+              parentName: 'ButtonProps',
+              defaultValue: '() => {}',
+              type: {
+                name: 'function',
+                raw: '(() => void)',
+              },
+            },
+          },
+        },
+        fileInfo: {
+          sloc: {
+            total: 81,
+            source: 45,
+            comment: 27,
+            single: 0,
+            block: 27,
+            mixed: 0,
+            empty: 9,
+            todo: 0,
+            blockEmpty: 0,
+          },
+          commits: [
+            {
+              hash: 'e9fc88d90f6f9541d8744530b49c8c595fbe7b99',
+              subject: 'docs: update tutorial stories',
+              authorName: 'atanasster',
+              authorDate: '2020-07-20T04:41:19.000Z',
+              authorEmail: 'atanasster@gmail.com',
+              committerName: 'atanasster',
+              committerDate: '2020-07-20T04:41:19.000Z',
+              committerEmail: 'atanasster@gmail.com',
+            },
+            {
+              hash: '3e9724f3d740da33bd277b03f8a83017159be114',
+              subject: 'docs: added starter example',
+              authorName: 'atanasster',
+              authorDate: '2020-07-19T04:05:43.000Z',
+              authorEmail: 'atanasster@gmail.com',
+              committerName: 'atanasster',
+              committerDate: '2020-07-19T04:05:43.000Z',
+              committerEmail: 'atanasster@gmail.com',
+            },
+          ],
+        },
+      },
+    });
+    expect(store.buildConfig).toMatchObject({
+      siteRoot: '/root/',
+      siteMap: {
+        pages: {
+          home: {
+            priority: 1,
+          },
+          index: {
+            priority: 0.8,
+          },
+          doc: {
+            priority: 0.5,
+          },
+        },
+      },
+      categories: ['author', 'tags'],
+      ignore: [
+        'readme.md',
+        'changelog.md',
+        'code_of_conduct.md',
+        'contributing.md',
+        'license.md',
+      ],
+      pages: {
+        story: {
+          basePath: 'docs/',
+          sideNav: {
+            storyPaths: true,
+            collapseSingle: true,
+          },
+          tabs: {},
+        },
+        blog: {
+          basePath: 'blogs/',
+        },
+        author: {
+          basePath: 'authors/',
+        },
+        page: {
+          basePath: 'pages/',
+        },
+        tags: {
+          basePath: 'tags/',
+        },
+      },
+      search: {
+        searchingModule: '@component-controls/search-fusejs',
+      },
+      bundleName: 'component-controls.js',
+      cssFileName: undefined,
+      stories: ['../src/docs/*.@(mdx|tsx)'],
+    });
   }, 50000);
 });
--- core/webpack-compile/tests/react-typescript.test.ts
+++ core/webpack-compile/tests/react-typescript.test.ts
@@ -11,10 +11,363 @@ expect.addSnapshotSerializer({
 describe('test typescript react settings', () => {
   it('compile', async () => {
     const { bundleName } = await compile({
+      logOptions: {
+        logLevel: 'none',
+      },
       presets: ['react', 'react-docgen-typescript'],
       configPath: path.resolve(__dirname, 'fixtures'),
     });
     const bundle = require(bundleName);
-    expect(bundle).toMatchSnapshot();
+    const store = bundle.stores[0];
+    expect(store).toMatchObject({
+      stories: {
+        overview: {
+          loc: {
+            start: {
+              column: 49,
+              line: 27,
+            },
+            end: {
+              column: 1,
+              line: 43,
+            },
+          },
+          name: 'overview',
+          id: 'overview',
+          arguments: [
+            {
+              value: [
+                {
+                  value: 'themeKey',
+                  name: 'themeKey',
+                  loc: {
+                    start: {
+                      line: 0,
+                      column: 3,
+                    },
+                    end: {
+                      line: 0,
+                      column: 11,
+                    },
+                  },
+                  usage: [
+                    {
+                      loc: {
+                        start: {
+                          line: 3,
+                          column: 16,
+                        },
+                        end: {
+                          line: 3,
+                          column: 24,
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+              loc: {
+                start: {
+                  line: 0,
+                  column: 1,
+                },
+                end: {
+                  line: 0,
+                  column: 13,
+                },
+              },
+            },
+          ],
+          controls: {
+            themeKey: {
+              type: 'options',
+              options: ['actionbar', 'toolbar', 'footer'],
+              value: 'actionbar',
+            },
+          },
+          source:
+            "({ themeKey }) => (\n  <Container>\n    <ActionBar\n      themeKey={themeKey}\n      actions={[\n        {\n          node: 'action 1',\n          onClick: () => console.log('clicked'),\n        },\n        {\n          node: <ExternalLink href=\"https://google.com\">google</ExternalLink>,\n          onClick: () => console.log('clicked'),\n        },\n      ]}\n    />\n  </Container>\n)",
+        },
+        link: {
+          loc: {
+            start: {
+              column: 29,
+              line: 53,
+            },
+            end: {
+              column: 1,
+              line: 63,
+            },
+          },
+          name: 'link',
+          id: 'link',
+          arguments: [],
+          source:
+            '() => (\n  <Container>\n    <ActionBar\n      actions={[\n        {\n          node: <ExternalLink href="https://google.com">google</ExternalLink>,\n        },\n      ]}\n    />\n  </Container>\n)',
+        },
+        order: {
+          loc: {
+            start: {
+              column: 30,
+              line: 65,
+            },
+            end: {
+              column: 1,
+              line: 82,
+            },
+          },
+          name: 'order',
+          id: 'order',
+          arguments: [],
+          source:
+            "() => (\n  <Container>\n    <ActionBar\n      actions={[\n        {\n          node: 'action 1',\n          onClick: () => console.log('clicked'),\n          order: 1,\n        },\n        {\n          node: <ExternalLink href=\"https://google.com\">google</ExternalLink>,\n          onClick: () => console.log('clicked'),\n          order: 0,\n        },\n      ]}\n    />\n  </Container>\n)",
+        },
+        override: {
+          loc: {
+            start: {
+              column: 33,
+              line: 84,
+            },
+            end: {
+              column: 1,
+              line: 104,
+            },
+          },
+          name: 'override',
+          id: 'override',
+          arguments: [],
+          source:
+            "() => (\n  <Container>\n    <ActionBar\n      actions={[\n        {\n          node: 'action 1',\n          onClick: () => console.log('clicked'),\n          id: 'copy',\n        },\n        {\n          node: <ExternalLink href=\"https://google.com\">google</ExternalLink>,\n          onClick: () => console.log('clicked'),\n        },\n        {\n          node: 'Copy',\n          id: 'copy',\n        },\n      ]}\n    />\n  </Container>\n)",
+        },
+        groupEnd: {
+          loc: {
+            start: {
+              column: 33,
+              line: 106,
+            },
+            end: {
+              column: 1,
+              line: 124,
+            },
+          },
+          name: 'groupEnd',
+          id: 'groupEnd',
+          arguments: [],
+          source:
+            "() => (\n  <Container>\n    <ActionBar\n      actions={[\n        {\n          node: 'item 1',\n        },\n        {\n          node: 'item 2',\n          group: '1',\n        },\n        {\n          node: 'item 3',\n          group: '1',\n        },\n      ]}\n    />\n  </Container>\n)",
+        },
+        groupStart: {
+          loc: {
+            start: {
+              column: 35,
+              line: 126,
+            },
+            end: {
+              column: 1,
+              line: 144,
+            },
+          },
+          name: 'groupStart',
+          id: 'groupStart',
+          arguments: [],
+          source:
+            "() => (\n  <Container>\n    <ActionBar\n      actions={[\n        {\n          node: 'item 1',\n          group: '1',\n        },\n        {\n          node: 'item 2',\n          group: '1',\n        },\n        {\n          node: 'item 3',\n        },\n      ]}\n    />\n  </Container>\n)",
+        },
+      },
+      doc: {
+        title: 'Components/ActionBar',
+        component: 'ActionBar',
+        category: 'Navigation',
+        componentsLookup: {
+          ActionBar: 'fded7dbaa855ea6869a794f877eab680',
+        },
+      },
+    });
+
+    expect(bundle.components).toMatchObject({
+      fded7dbaa855ea6869a794f877eab680: {
+        name: 'ActionBar',
+        from: './ActionBar',
+        externalDependencies: {
+          react: [
+            {
+              name: 'FC',
+              importedName: 'FC',
+            },
+            {
+              name: 'useMemo',
+              importedName: 'useMemo',
+            },
+          ],
+          'theme-ui': [
+            {
+              name: 'Box',
+              importedName: 'Box',
+            },
+            {
+              name: 'Button',
+              importedName: 'Button',
+            },
+            {
+              name: 'jsx',
+              importedName: 'jsx',
+            },
+          ],
+        },
+        localDependencies: {
+          './utils': [
+            {
+              name: 'getSortedActions',
+              importedName: 'getSortedActions',
+            },
+            {
+              name: 'ActionItems',
+              importedName: 'ActionItems',
+            },
+          ],
+          '../Link': [
+            {
+              name: 'Link',
+              importedName: 'Link',
+            },
+          ],
+        },
+        importedName: 'ActionBar',
+        jsx: [
+          {
+            children: [
+              {
+                children: [],
+                name: 'Link',
+                attributes: ['variant', 'href', 'aria-label'],
+                from: '../Link',
+                importedName: 'Link',
+              },
+              {
+                children: [],
+                name: 'Button',
+                attributes: ['variant', 'onClick', 'aria-label'],
+                from: 'theme-ui',
+                importedName: 'Button',
+              },
+            ],
+            name: 'Box',
+            attributes: ['key', 'sx'],
+            from: 'theme-ui',
+            importedName: 'Box',
+          },
+          {
+            children: [
+              {
+                children: [],
+                name: 'Box',
+                attributes: ['variant'],
+                from: 'theme-ui',
+                importedName: 'Box',
+              },
+            ],
+            name: 'Box',
+            attributes: ['variant'],
+            from: 'theme-ui',
+            importedName: 'Box',
+          },
+        ],
+        fileName: 'ActionBar.tsx',
+        info: {
+          tags: {},
+          description:
+            'A strip of actions to be attached to a container.\nThe action items contain the labels and click event handlers.\nActions can accept an order prop, and can also overwrite default actions.',
+          displayName: 'ActionBar',
+          methods: [],
+          props: {
+            actions: {
+              description: 'collection of action items',
+              parentName: 'ActionBarProps',
+              defaultValue: '[]',
+              type: {
+                name: 'ActionItems',
+                raw: 'ActionItems',
+              },
+            },
+            themeKey: {
+              description:
+                "custom layouts from the theme, defaults to 'actionbar'",
+              parentName: 'ActionBarProps',
+              defaultValue: 'actionbar',
+              type: {
+                name: 'string',
+                raw: 'string',
+              },
+            },
+          },
+        },
+        fileInfo: {
+          sloc: {
+            total: 78,
+            source: 62,
+            comment: 13,
+            single: 0,
+            block: 13,
+            mixed: 0,
+            empty: 3,
+            todo: 0,
+            blockEmpty: 0,
+          },
+        },
+      },
+    });
+
+    expect(bundle.buildConfig).toMatchObject({
+      siteRoot: '/',
+      siteMap: {
+        pages: {
+          home: {
+            priority: 1,
+          },
+          index: {
+            priority: 0.8,
+          },
+          doc: {
+            priority: 0.5,
+          },
+        },
+      },
+      categories: ['author', 'tags'],
+      ignore: [
+        'readme.md',
+        'changelog.md',
+        'code_of_conduct.md',
+        'contributing.md',
+        'license.md',
+      ],
+      pages: {
+        story: {
+          basePath: 'docs/',
+          sideNav: {
+            storyPaths: true,
+            collapseSingle: true,
+          },
+        },
+        blog: {
+          basePath: 'blogs/',
+        },
+        author: {
+          basePath: 'authors/',
+        },
+        page: {
+          basePath: 'pages/',
+        },
+        tags: {
+          basePath: 'tags/',
+        },
+      },
+      search: {
+        searchingModule: '@component-controls/search-fusejs',
+      },
+      bundleName: 'component-controls.js',
+      stories: [
+        '../../../../ui/components/src/ActionBar/*.stories.(js|jsx|tsx|mdx)',
+      ],
+    });
   }, 30000);
 });
