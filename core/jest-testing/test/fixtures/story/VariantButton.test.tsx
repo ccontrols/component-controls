0b3d2f1b777c859cc020b2c27dd2cf7ad53c5817
atanasster <atanasster@gmail.com>
2021-03-21 23:37:33 -0400

    test: inline snapshots

--- core/jest-testing/test/fixtures/story/VariantButton.test.tsx
+++ core/jest-testing/test/fixtures/story/VariantButton.test.tsx
@@ -7,13 +7,40 @@ describe('VariantButton', () => {
   test('primary', async () => {
     const { getByText, asFragment, getByRole } = render(<Primary />);
     fireEvent.click(getByText('Primary'));
-    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchInlineSnapshot(`
+<DocumentFragment>
+  <button
+    style="color: rgb(242, 242, 242); background-color: rgb(47, 128, 237); font-size: 18px; padding: 10px 20px; border-radius: 8px; border: 1px solid #000000; display: flex; flex-direction: row; align-items: center;"
+  >
+    <div
+      role="label"
+      style="padding: 0px 10px;"
+    >
+      Primary
+    </div>
+  </button>
+</DocumentFragment>
+`);
     expect(getByRole('label')).toHaveTextContent('Primary');
   });
 
   test('disabled', async () => {
     const { asFragment, getByRole } = render(<Disabled />);
-    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchInlineSnapshot(`
+  <DocumentFragment>
+    <button
+      disabled=""
+      style="color: rgb(130, 130, 130); background-color: rgb(224, 224, 224); font-size: 18px; padding: 10px 20px; border-radius: 8px; border: 1px solid #000000; display: flex; flex-direction: row; align-items: center;"
+    >
+      <div
+        role="label"
+        style="padding: 0px 10px;"
+      >
+        Disabled
+      </div>
+    </button>
+  </DocumentFragment>
+  `);
     expect(getByRole('label')).toHaveTextContent('Disabled');
     expect(getByRole('button')).toHaveAttribute('disabled');
   });
