82e3ee5d9cce837c25b1571dacd8a5aa316a029a
atanasster <atanasster@gmail.com>
2021-04-20 13:13:20 -0400

    docs: execute tests for starter story

--- ui/blocks/src/ComponentJSX/ComponentJSX.tsx
+++ ui/blocks/src/ComponentJSX/ComponentJSX.tsx
@@ -20,7 +20,6 @@ const NAME = 'component_jsx';
 export const ComponentJSX: FC<ComponentJSXProps> = fullProps => {
   const props = useCustomProps<ComponentJSXProps>(NAME, fullProps);
   const component = useComponent({ of: props.of });
-  console.log(component);
   if (!component?.jsx?.length) {
     return null;
   }
