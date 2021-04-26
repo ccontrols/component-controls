0f22572986e6e390346ab5769c283506018fed01
atanasster <atanasster@gmail.com>
2021-04-26 15:04:47 -0400

    fix: typescript ally imports

--- plugins/cc-cli/templates/accessibility/import/import.ts.js
+++ plugins/cc-cli/templates/accessibility/import/import.ts.js
@@ -1,3 +1,3 @@
-import { run, AxeResults } = require('axe-core');
-import { reactRunDOM } = require('@component-controls/test-renderers');
+import { run, AxeResults }from 'axe-core';
+import { reactRunDOM } from '@component-controls/test-renderers';
 import '@component-controls/jest-axe-matcher';
