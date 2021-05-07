4e73a3d1b867123feaa18e648abd1910dd191484
atanasster <atanasster@gmail.com>
2021-05-07 23:30:55 +0300

    test: enable data-driven testing

---
+++ ui/app/src/AppError/AppError.data.ts
@@ -0,0 +1,19 @@
+export default {
+  overview: {
+    '0': {
+      error: 'Dr. Eli Beatty',
+    },
+    '1': {
+      error: 'Mr. Russell Volkman',
+    },
+    '2': {
+      error: 'Dr. Zachary Kuphal',
+    },
+    '3': {
+      error: 'Mrs. Freeda Nikolaus',
+    },
+    '4': {
+      error: 'Emilio Kozey',
+    },
+  },
+};
