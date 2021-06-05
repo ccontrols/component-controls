0725be7035bdfa0e046c491cae9d483eebb760dd
atanasster <atanasster@gmail.com>
2021-06-05 12:53:29 +0300

    chore: remove old types

--- misc/jsdoc-extract/src/utils.ts
+++ misc/jsdoc-extract/src/utils.ts
@@ -7,53 +7,6 @@ export interface JSDocExample {
   caption?: string;
   content?: string;
 }
-export type TSType =
-  | 'object'
-  | 'string'
-  | 'number'
-  | 'boolean'
-  | 'union'
-  | 'enum'
-  | 'tuple'
-  | 'rest'
-  | 'undefined'
-  | 'null'
-  | 'function'
-  | 'void'
-  | 'class'
-  | 'interface'
-  | 'type'
-  | 'array'
-  | 'any'
-  | 'reference'
-  | 'predicate'
-  | 'index'
-  | 'unknown';
-
-export interface JSDocType {
-  name?: string;
-  kind?: string;
-  namepath?: string;
-  memberof?: string;
-  type?: TSType;
-  fnType?: 'constructor' | 'getter' | 'setter' | 'predicate';
-  description?: string;
-  parameters?: JSDocType[];
-  properties?: JSDocType[];
-  inherits?: JSDocType[];
-  returns?: JSDocType;
-  fires?: { data: string }[];
-  see?: string[];
-  examples?: JSDocExample[];
-  value?: any;
-  optional?: boolean;
-  readonly?: boolean;
-  abstract?: boolean;
-  visibility?: 'private' | 'protected' | 'public';
-  static?: boolean;
-  deprecated?: string;
-}
-
 export type JSImport = {
   name?: string;
   module: string;
