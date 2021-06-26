4d1999437627b43171bb3706195e19f0ee20110f
atanasster <atanasster@gmail.com>
2021-06-26 17:13:10 +0300

    chore: move url function to exports

--- ui/blocks/src/utils/url.ts
+++ ui/blocks/src/utils/url.ts
@@ -1,3 +1,5 @@
+import queryString from 'query-string';
+
 export const getURL = (): URL | undefined => {
   let pageURL = undefined;
   if (typeof window !== 'undefined') {
@@ -8,4 +10,36 @@ export const getURL = (): URL | undefined => {
     }
   }
   return pageURL ? new URL(pageURL) : undefined;
+};
+export const getUpdatedUrlParams = (
+  paremName: string,
+  value: string,
+): string | undefined => {
+  const url = getURL();
+  if (url) {
+    const parsedParams = queryString.parse(url.search);
+    if (value) {
+      parsedParams[paremName] = value;
+    } else {
+      delete parsedParams.paremName;
+    }
+    const strValues = queryString.stringify(parsedParams);
+    const newURL = `${url.protocol}//${url.host}${url.pathname}${
+      strValues ? `?${strValues}` : ''
+    }${url.hash ? `#${url.hash}` : ''}`;
+    return newURL;
+  }
+  return undefined;
+};
+
+export const getUrlParams = (paremName: string): any => {
+  const url = getURL();
+  if (url) {
+    const parsedParams = queryString.parse(url.search);
+    const params = parsedParams[paremName];
+    if (params && typeof params === 'string') {
+      return unescape(params);
+    }
+  }
+  return undefined;
 };
