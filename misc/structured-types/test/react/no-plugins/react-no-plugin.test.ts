25eae36bf06f136261cea0d91f94e21e19bf5947
atanasster <atanasster@gmail.com>
2021-07-07 11:10:01 +0300

    test: fix fixture path

--- misc/structured-types/test/react/no-plugins/react-no-plugin.test.ts
+++ misc/structured-types/test/react/no-plugins/react-no-plugin.test.ts
@@ -2,9 +2,7 @@ import path from 'path';
 import { parseFiles } from '../../../src/index';
 describe('react-no-plugin', () => {
   it('react types', () => {
-    const result = parseFiles([
-      path.resolve(__dirname, 'fixtures', 'react-types'),
-    ]);
+    const result = parseFiles([path.resolve(__dirname, 'react-types.tsx')]);
     expect(result).toMatchSnapshot();
   });
 });
